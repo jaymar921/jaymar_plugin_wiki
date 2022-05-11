ecb9a1d83f53ac5ff10afa4c83816557d7e27ec2
JayMar <72720429+jaymar921@users.noreply.github.com>
2022-05-11 08:32:12 +0800

    Add files via upload

---
+++ js/ce3.js
@@ -0,0 +1,19 @@
+
+function downloadCE3(){
+
+	window.open('https://www.youtube.com/c/jaymar921')
+
+	
+	setTimeout(openCE3Link,5000)
+	
+	
+}
+
+function openCE3Link(){
+	//let youtube = prompt('Have you subscribed to jaymar? Y/N')
+	//switch(youtube){
+	//	case "Y": case "y":
+	//		location.href = 'https://drive.google.com/file/d/1ozwwo4B5oU2ncmST4Kwmap_Pyr594RYo/view?usp=sharing'
+	//}
+	location.href = 'https://drive.google.com/file/d/1ozwwo4B5oU2ncmST4Kwmap_Pyr594RYo/view?usp=sharing'
+}
