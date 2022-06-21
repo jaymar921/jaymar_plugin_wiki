c833a411e90ea9a544a8b89ec96008863abd867e
JayMar <72720429+jaymar921@users.noreply.github.com>
2022-06-21 21:17:11 +0800

    Update ce3.js

--- js/ce3.js
+++ js/ce3.js
@@ -15,5 +15,5 @@ function openCE3Link(){
 	//	case "Y": case "y":
 	//		location.href = 'https://drive.google.com/file/d/1ozwwo4B5oU2ncmST4Kwmap_Pyr594RYo/view?usp=sharing'
 	//}
-	location.href = 'https://drive.google.com/file/d/1ozwwo4B5oU2ncmST4Kwmap_Pyr594RYo/view?usp=sharing'
+	location.href = 'https://www.spigotmc.org/resources/89793/'
 }
